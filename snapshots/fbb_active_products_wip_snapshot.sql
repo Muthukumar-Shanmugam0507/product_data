a750c8a1d1df60214ad0b3d8275dcfed148b6c51
Muthukumar Shanmugam <muthukumar.shanmugam@wipro.com>
2026-09-11 02:04:20 +0000

    commiting

--- snapshots/fbb_active_products_wip_snapshot.sql
+++ snapshots/fbb_active_products_wip_snapshot.sql
@@ -1,8 +1,9 @@
-{% snapshot snapshot_fbb_active_products %}
+{% snapshot snapshot_fbb_active_products_wip %}
 
 {{
     config(
         target_database='DS_PROJECTS',
+        target_schema='PRODUCT_DATA_MANAGEMENT_WIP',
         unique_key='HASH_KEY',
         strategy='check',
         check_cols=['HASH_KEY'],
